4859cb6b0782d0d8578e13c83d7ed93c57f4f4ed
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2025-04-15 11:42:48 +0200

    Update dependencies and add on_audio_query_android plugin
    This commit includes the following changes:
    
    1. Updated the `pubspec.yaml` file to bump the versions of `flutter_bloc` and `get_it` dependencies, and added a dependency override for `on_audio_query_android`.
    2. Added the `on_audio_query_android` plugin, which includes the necessary Android implementation for querying audio data.
    3. Introduced various utility classes and methods for handling audio queries, permissions, and data retrieval.
    4. Updated the `pubspec.lock` file to reflect the changes in dependencies and their versions.
    
    These updates enhance the app's audio querying capabilities and ensure compatibility with the latest versions of the dependencies.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import just_audio
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
